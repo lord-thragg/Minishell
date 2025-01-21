b30512bc55cc2f58cc4653c8e746add20b8ee641
Lucas Luzurier <luluzuri@bess-f2r5s11.clusters.42paris.fr>
2025-01-21 08:16:57 +0100

    Added cd command

--- obj/main.d
+++
@@ -1,3 +0,0 @@
-obj/main.o: src/main.c includes/minishell.h
-
-includes/minishell.h:
--- obj/signals/signal_handling.d
+++
@@ -1,4 +0,0 @@
-obj/signals/signal_handling.o: src/signals/signal_handling.c \
-  includes/minishell.h
-
-includes/minishell.h:
