d2de1398b28cae68cb230897a1d2b7ed98eadd04
luluzuri <luluzuri@student.42.fr>
2025-02-17 17:32:08 +0100

    test

--- src/parsing/command.c
+++ src/parsing/command.c
@@ -6,7 +6,7 @@
 /*   By: luluzuri <luluzuri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 09:52:14 by luluzuri          #+#    #+#             */
-/*   Updated: 2025/02/16 10:27:50 by luluzuri         ###   ########.fr       */
+/*   Updated: 2025/02/17 17:30:50 by luluzuri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,14 @@ int	count_token(t_token *token)
 t_cmd	*token_to_command(t_token *token)
 {
 	t_cmd	*head;
+	t_cmd	*ncmd;
+	int		i;
+
+	i = 0;
+	head = NULL;
+	ncmd = NULL;
+	while (token)
+	{
+		ncmd = create_command(&token)
+	}
 }
