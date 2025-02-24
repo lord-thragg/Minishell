67fa49779f6d10205b745bcbeff2ba2ddd642e0c
Lucas Luzurier <luluzuri@bess-f2r4s14.clusters.42paris.fr>
2025-02-24 09:05:55 +0100

    handling heredoc

--- obj/main.d
+++
@@ -1,5 +0,0 @@
-obj/main.o: src/main.c includes/minishell.h libft/libft.h
-
-includes/minishell.h:
-
-libft/libft.h:
--- obj/parsing/utils_commands.d
+++
@@ -1,6 +0,0 @@
-obj/parsing/utils_commands.o: src/parsing/utils_commands.c \
-  includes/minishell.h libft/libft.h
-
-includes/minishell.h:
-
-libft/libft.h:
